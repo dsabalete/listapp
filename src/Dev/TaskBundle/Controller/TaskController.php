f01659fca85dc565bcff6509057a0cbe58226c99
David Sabalete <dsabalete@gmail.com>
2014-11-26 23:49:45 +0100

    form_enctype deprecated

--- src/Dev/TaskBundle/Controller/TaskController.php
+++ src/Dev/TaskBundle/Controller/TaskController.php
@@ -315,7 +315,8 @@ class TaskController extends Controller
         }
 
         $editForm = $this->createEditCompleteForm($entity);
-        $editForm->handleRequest($request);
+        //$editForm->handleRequest($request);
+        $editForm->bind($request);
 
         if ($editForm->isValid()) {
             $em->persist($entity);
